cfb98e0ce7684b060b18273ef92733674a9b6336
David Parsley <parsley@linuxjedi.org>
2018-08-16 19:22:29 +0000

    Working on gopherci

--- jobs/gopherci.py
+++ jobs/gopherci.py
@@ -9,3 +9,21 @@ bot = Robot()
 
 from yaml import load
 
+repository = sys.argv.pop(0)
+branch = sys.argv.pop(0)
+
+repofile = "%s/conf/repodata.yaml" % os.getenv("GOPHER_CONFIGDIR")
+repodata = load(repofile)
+
+if repository in repodata:
+    repoconf = repodata[repository]
+    if "type" in repoconf:
+        repotype = repoconf["type"]
+    else
+        bot.Say("No 'type' specified for %s" % repository)
+        exit()
+else:
+    bot.Say("Repository '%s' not found in 'repositories.yaml'" % repository)
+    exit()
+
+bot.Say("Found '%s' in 'repositories.yaml', type '%s'" % (repository, repotype))
